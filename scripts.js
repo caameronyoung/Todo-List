8f7d3b0a920559a6e8e0db526acf4a897424b512
Cameron Young <52024862+caameronyoung@users.noreply.github.com>
2024-03-21 15:46:16 +1100

    functionality for button and text changing colour

--- scripts.js
+++ scripts.js
@@ -1,27 +1,51 @@
 //dateTime variables to increase usability of todo list each day
 const todaysDate = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+let completed = false;
 let day = days[todaysDate.getDay()].toString();
 let date = todaysDate.getDate(); 
 let month = todaysDate.getMonth() + 1;
 let year = todaysDate.getFullYear().toString().substr(-2).toString();
-console.log(day);
 
-//constant getters and setters for drawing the checkboxes of my todo list
+
+
+
+//printing date to the page
+document.getElementById('dateHolder').innerText += ' ' + day + ' ' + date + '/' + month + '/' + year;
+
+
+//task defintions
+let chore = document.getElementById('task');
+console.log(chore);
+
+
+
+
+
+//Event listener for clicking check boxes
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(20,20,30,30);
+let clicked = false;
 
-function drawShape()
+//event listener so if clicked it toggles the colour yep yep
+
+canvas.addEventListener('click', testFunction)
+function testFunction()
 {
-    ctx.beginPath();
-    ctx.moveTo(20,20);
-    ctx.lineTo(100,20);
-    ctx.lineTo(175,100);
-    ctx.lineTo(20,100);
-    ctx.lineTo(20,20);
-    ctx.stroke();
+    if (!clicked)
+    {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(20,20,30,30);
+        clicked = true;
+        chore.style.color = 'green';
+    }
+    else if (clicked)
+    {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(20,20,30,30);
+        clicked =  false;
+        chore.style.color = 'red';
+    }
 }
-
-
-document.getElementById('dateHolder').innerText += ' ' + day + ' ' + date + '/' + month + '/' + year;
